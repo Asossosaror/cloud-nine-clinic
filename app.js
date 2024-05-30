4958bee14493e229db5a605b80d71009d2160dd0
Assar Grimlund (Lenovo Laptop) <assar.grimlund@gmail.com>
2024-05-30 17:55:37 +0200

    add book-btn and apply 'scrolled'-class on it

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const checkScrollStatus = (header, navbarLogo, bookBtn) => {
+    if(window.scrollY > 0) {
+        header.classList.add('scrolled');
+        navbarLogo.classList.add('scrolled');
+        bookBtn.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+        navbarLogo.classList.remove('scrolled');
+        bookBtn.classList.remove('scrolled');
+    }
+}
+
 document.addEventListener("DOMContentLoaded", evt => {
     const header = document.getElementById('navbar');
     const navbarLogo = document.getElementById('navbar-logo');
+    const bookBtn = document.getElementById('book-btn-container');
+
+    checkScrollStatus(header, navbarLogo, bookBtn);
 
     window.onscroll = () => {
-        if(window.scrollY > 0) {
-            header.classList.add('scrolled');
-            navbarLogo.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
-            navbarLogo.classList.remove('scrolled');
-        }
+        checkScrollStatus(header, navbarLogo, bookBtn);
     }
 });
